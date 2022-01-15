28e1c41711b522fc82ba3cd4f89090ffafdb9fda
Alan Tavares <alan_tj@hotmail.com.br>
2022-01-15 16:04:56 -0300

    create project

---
+++ aplicacao_crud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aplicacao_crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
