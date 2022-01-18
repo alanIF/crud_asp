20f1df5f5726328fd076a4400326de5209456f26
Alan Tavares <alan_tj@hotmail.com.br>
2022-01-18 17:37:50 -0300

    validator

---
+++ SquadManage/SquadManage/Validator/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SquadManage.Models;
+
+namespace SquadManage.Validator
+{
+    public class UserValidator: AbstractValidator<UserViewModel>
+    {
+        public UserValidator() { 
+            RuleFor(user=> user.Email).NotNull().WithMessage("Nao pode ser vazio!") ;
+            RuleFor(user => user.Email).EmailAddress().WithMessage("Email incorreto!");
+
+            RuleFor(user => user.Password).NotNull().WithMessage("Nao pode ser vazio!");
+            RuleFor(user => user.Username).NotNull().WithMessage("Nao pode ser vazio!") ;
+
+        }
+    }
+}
